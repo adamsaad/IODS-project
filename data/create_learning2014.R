12a0abc359c916bf6275f13822812eb5f9fff012
Adam Saada <saada.adam@gmail.com>
2018-11-02 18:55:43 +0200

    add create_learning2014 file with data

---
+++ data/create_learning2014.R
@@ -0,0 +1,11 @@
+#Adam Saada
+#2.11.2018
+#Excercise 2
+
+JYTOPKYS3_data <- read_delim("~/IODS-project/data/JYTOPKYS3-data.txt", 
+                               +     "\t", escape_double = FALSE, trim_ws = TRUE)
+
+View(JYTOPKYS3_data)
+
+#Numeric data with explanatory variable as M or F
+
